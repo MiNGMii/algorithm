95ddf015949347ec7394759bfb4f0e612374911b
M12022\student <sak9994@gmail.com>
2018-04-19 16:06:27 +0900

    modify answer for 1463

--- baekjoon/src/bj1463.java
+++ baekjoon/src/bj1463.java
@@ -8,23 +8,7 @@ public class bj1463 {
 		System.out.print("X = ");
 		int x = sc.nextInt();
 		
-		int cnt = 0;
-		
-		while (x > 1) {
-			
-			if (x % 3 == 0)
-				x /= 3;
-			else if (x % 2 == 0)
-				x /= 2;
-			else
-				x -= 1;
-			
-			cnt++;
-		}
-		
-		System.out.println(cnt);
-		
-		/*int[] cnt = new int[x + 1];
+		int[] cnt = new int[x + 1];
 		
 		// x가 1이거나 0일 경우 연산의 수는 0번이므로 2부터 시작
 		for (int i = 2; i <= x; i++) {
@@ -36,7 +20,7 @@ public class bj1463 {
             if(i % 3 == 0 && cnt[i] > cnt[i/3] + 1)
                 cnt[i] = cnt[i / 3] + 1;
 
-        }*/
+        }
 		
 	}
 
