19514e652cc504fbac04111d633c5af11d52693b
M12022\student <sak9994@gmail.com>
2018-05-17 21:15:39 +0900

    A binary gap
    Task description
    A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
    
    For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
    
    Write a function:
    
    class Solution { public int solution(int N); }
    
    that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
    
    For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
    
    Assume that:
    
    N is an integer within the range [1..2,147,483,647].
    Complexity:
    
    expected worst-case time complexity is O(log(N));
    expected worst-case space complexity is O(1).
    Copyright 2009–2018 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.

---
+++ codility/src/BinaryGap.java
@@ -0,0 +1,29 @@
+
+public class BinaryGap {
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution(1041));
+	}
+	
+	public static int solution(int N) {
+        
+		String input = Integer.toBinaryString(N);
+		int[] arr = new int[input.length()];
+		int pos = 0;
+		
+		for (int i = 0; i < input.length(); i++) {
+			
+			if(input.charAt(i) == '1')	arr[pos++] = i;
+		}
+		
+		int result = arr[1] - arr[0];
+		
+		for (int i = 2; i < arr.length; i++) {
+			
+			if (result < arr[i] - arr[i-1])	result = arr[i] - arr[i-1];
+		}
+		
+		return result - 1;
+    }
+}
