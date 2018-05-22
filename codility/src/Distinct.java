739623105f725d0e5532f1d41420c31a19bb9b1c
MIA\user <sak9994@gmail.com>
2018-05-22 16:46:10 +0900

    Distinct
    Write a function
    
    class Solution { public int solution(int[] A); }
    
    that, given an array A consisting of N integers, returns the number of distinct values in array A.
    
    Assume that:
    
    N is an integer within the range [0..100,000];
    each element of array A is an integer within the range [−1,000,000..1,000,000].
    For example, given array A consisting of six elements such that:
    
     A[0] = 2    A[1] = 1    A[2] = 1
     A[3] = 2    A[4] = 3    A[5] = 1
    the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
    
    Complexity:
    
    expected worst-case time complexity is O(N*log(N));
    expected worst-case space complexity is O(N) (not counting the storage required for input arguments).
    Copyright 2009–2018 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.

---
+++ codility/src/Distinct.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Distinct {
+	
+	public static void main(String[] args) {
+		int[] A = {2, 1, 1, 2, 3, 1};
+		System.out.println(solution(A));
+	}
+	
+	public static int solution(int[] A) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < A.length; i++) {
+			
+			if (map.get(A[i]) == null)
+				map.put(A[i], 1);
+		}
+		
+		return map.size();
+	}
+}
