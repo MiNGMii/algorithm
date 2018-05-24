77f4d2a649ecef4695dda81960fecd55abe065d1
M12022\student <sak9994@gmail.com>
2018-05-24 13:37:21 +0900

    MinAvgTwoSlice 수

--- codility/src/MinAvgTwoSlice.java
+++ codility/src/MinAvgTwoSlice.java
@@ -6,45 +6,30 @@ public class MinAvgTwoSlice {
 		System.out.println(solution(A));
 	}
 	
-	// 답은 맞지만 시간복잡도 초과 ^^..!
 	public static int solution(int[] A) {
+		int answer = 0;
+		double avg = Double.MAX_VALUE;
 		
-		double[][] arr = new double[A.length - 1][A.length];
-		double min = (A[0] + A[1]) / 2;
-		int minKey = 0;
-		/*System.out.println("min : " + min);*/
-		
-		for (int i = 0; i < A.length - 1; i++) {
-			arr[i][i + 1] = A[i] + A[i + 1];
+		for (int i = 0; i < A.length - 2; i++) {
+			double avg2 = (A[i] + A[i + 1]) / 2.0;
+			double avg3 = (A[i] + A[i + 1] + A[i + 2]) / 3.0;
 			
-			for (int j = i + 2; j < A.length; j++) {
-				arr[i][j] = arr[i][j - 1] + A[j];
+			if (avg > Math.min(avg2, avg3)) {
+				avg = Math.min(avg2, avg3);
+				answer = i;
 			}
+			
+			System.out.println(avg2);
+			System.out.println(avg3);
+			System.out.println(avg + "," + answer);
 		}
 		
-		for (int i = 0; i < A.length - 1; i++) {
-			
-			for (int j = i + 1; j < A.length; j++) {
-				arr[i][j] /= (j - i + 1);
-				/*System.out.println(arr[i][j]);*/
-			}
+		if (avg > (A[A.length - 2] + A[A.length - 1]) / 2.0) {
+			avg = (A[A.length - 2] + A[A.length - 1]) / 2.0;
+			answer = A.length - 2;
 		}
 		
-		for (int i = 0; i < A.length - 1; i++) {
-			
-			for (int j = i + 1; j < A.length; j++) {
-				
-				if (min > arr[i][j]) {
-					min = arr[i][j];
-					minKey = i;
-					
-				/*System.out.println("min: " + min);
-				System.out.println("minKey: " + minKey);*/
-				}
-			}
-		}
-		
-		return minKey;
+		return answer;
 	}
 
 }
