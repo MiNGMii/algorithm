5aa9b7e4297cb3af78b906a6547694677fd409b0
M12022\student <sak9994@gmail.com>
2018-05-24 11:06:31 +0900

    GenomicRangeQuery 수정

--- codility/src/GenomicRangeQuery.java
+++ codility/src/GenomicRangeQuery.java
@@ -8,37 +8,52 @@ public class GenomicRangeQuery {
 		int[] Q = {4, 5, 6};
 		System.out.println(Arrays.toString(solution(S, P, Q)));
 	}
-	
-	// 답은 맞지만 시간복잡도 초과..
+
 	public static int[] solution(String S, int[] P, int[] Q) {
-        
-		char[] input = S.toCharArray();
-		
-		char dna[] = new char[P.length];
-		
-		for (int i = 0; i < P.length; i++) {
-			char temp = 'T';
+		int[][] arr = new int[S.length()][4];
+		int[] result = new int[P.length];
+
+		for (int i = 0; i < S.length(); i++) {
 			
-			for (int j = P[i]; j <= Q[i]; j++) {
-				
-				if (temp > input[j])	temp = input[j];
+			switch (S.charAt(i)) {
+				case 'A':
+					arr[i][0] = 1;
+					break;
+				case 'C':
+					arr[i][1] = 1;
+					break;
+				case 'G':
+					arr[i][2] = 1;
+					break;
+				default:
+					arr[i][3] = 1;
+					break;
 			}
-			
-			dna[i] = temp;
-			
 		}
 		
-		int[] result = new int[dna.length];
-		
-		for (int i = 0; i < dna.length; i++) {
+		for (int i = 1; i < S.length(); i++) {
 			
-			if (dna[i] == 'A')		result[i] = 1;
-			else if (dna[i] == 'C')	result[i] = 2;
-			else if (dna[i] == 'G')	result[i] = 3;
-			else					result[i] = 4;
+			for (int j = 0; j < 4; j++)
+				arr[i][j] += arr[i - 1][j];
+		}
+
+		for (int i = 0; i < P.length; i++) {
+
+			for (int j = 0; j < 4; j++) {
+				int sub = 0;
+				
+				if (P[i] - 1 >= 0)
+					sub = arr[P[i] - 1][j];
+				
+				if (arr[Q[i]][j] - sub > 0) {
+					result[i] = j + 1;
+					break;
+				}
+			}
+
 		}
 		
 		return result;
-    }
-	
+	}
+
 }
