6f5b4d7a4c62bb29df57d741a1fdd7cb8c060565
M12022\student <sak9994@gmail.com>
2018-05-25 13:33:45 +0900

    자연수 뒤집어 배열로 만들기
    자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
    
    제한 조건
    n은 10,000,000,000이하인 자연수입니다.

---
+++ programers/src/MakeArray.java
@@ -0,0 +1,15 @@
+
+public class MakeArray {
+
+	public int[] solution(long n) {
+		int length = Long.toString(n).length();
+		int[] answer = new int[length];
+		
+		for (int i = 0; i < length; i++) {
+			answer[i] = (int) (n % 10);
+			n /= 10;
+		}
+		
+		return answer;
+	}
+}
