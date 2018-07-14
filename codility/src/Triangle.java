c7a4eb4a92f15a9528ffe66fc552a6ae72c32a8a
MIA\user <sak9994@gmail.com>
2018-07-14 16:33:59 +0900

    Triangle

---
+++ codility/src/Triangle.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Triangle {
+	
+	public int solution(int[] A) {
+		
+		Arrays.sort(A);
+		
+		for (int i = 0; i < A.length - 2; i++) {
+			
+			if (A[i] + A[i + 1] > A[i + 2]) 
+				return 1;
+		}
+		
+		return 0;
+	}
+}
