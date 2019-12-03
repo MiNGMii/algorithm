ef882127ee466d637c92e33cd7563a1e147dc1e0
MiNGMii <sak9994@gmail.com>
2019-12-03 22:16:47 +0900

    TwoSum

--- leetCode/src/TwoSum.java
+++ leetCode/src/TwoSum.java
@@ -7,27 +7,24 @@ public class TwoSum {
 		TwoSum twoSum = new TwoSum();
 		int[] A =  {2,7,11,15};
 		int[] B =  {3,2,4};
+		int[] C =  {3,3};
 		System.out.println(Arrays.toString(twoSum.twoSum(A, 9)));
 		System.out.println(Arrays.toString(twoSum.twoSum(B, 6)));
+		System.out.println(Arrays.toString(twoSum.twoSum(C, 6)));
 	}
 	
 	public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
-        ArrayList<Integer> list = new ArrayList<Integer>();
         
         for (int i = 0; i < nums.length; i++) {
-        	final int me = nums[i];
-        	final int temp = target - nums[i];
-        	//System.out.println(temp + " " + Arrays.stream(nums).anyMatch(x -> x == temp && x != me));
-            if (Arrays.stream(nums).anyMatch(x -> x == temp && x != me)) {
-            	list.add(i);
-            }
-                
-        }
-        
-        if (list.size() >= 2) {
-        	answer[0] = list.get(0);
-            answer[1] = list.get(1);
+        	for (int j = 0; j < nums.length; j++) {
+        		if (i == j) continue;
+        		if (nums[i] + nums[j] == target) {
+        			answer[0] = i;
+        			answer[1] = j;
+        			return answer;
+        		}
+        	}
         }
         
         return answer;
